c50a56966c03e524a334d463d7e7ee89326ede5e
YinjianChen <YinjianChen02@gmail.com>
2017-03-03 23:47:13 +0800

    完成滤镜取图

--- ArtRecorder/Sections/Video/Controller/VideoViewController.m
+++ ArtRecorder/Sections/Video/Controller/VideoViewController.m
@@ -16,8 +16,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blackColor];
+//    NSString *filePath = [[NSBundle mainBundle] pathForResource:
+//                          @"1" ofType:@"jpg"
+//                          ];
+//    
+//    UIImage *image = [UIImage imageWithContentsOfFile:filePath];
+//    UIImageView *imgV = [[UIImageView alloc]initWithFrame:self.view.frame];
+//    imgV.contentMode = UIViewContentModeScaleAspectFit;
+//    
+//    GPUImageSketchFilter * Filter1 = [[GPUImageSketchFilter alloc] init];
+//
+//    
+//    
+//    
+//    GPUImagePicture *pic = [[GPUImagePicture alloc] initWithImage:image];
+//    //添加Target
+//    [pic addTarget:Filter1];
+//    [pic processImage];
+//    [Filter1 useNextFrameForImageCapture];
+//    
+//    image = [Filter1 imageFromCurrentFramebuffer];
+//    imgV.image = image;
+//    
+//    [self.view addSubview:imgV];
     
-    self.view.backgroundColor = [UIColor blueColor];
 }
 
 
